5e2b3c3f65956e480d31d03ac31e5b646e3388cd
Karthikeyan <karthikeyanagaram2004@gmail.com>
2026-05-15 22:40:12 +0530

    fixed backend root route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const cors = require('cors');
+
 require('dotenv').config();
 
 const employeeRoutes =
@@ -13,10 +15,13 @@ const authRoutes =
 
 const app = express();
 
-const cors = require('cors')
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
+
+/* ===================================== */
+/* ROUTES */
+/* ===================================== */
 
 app.use(
   '/api/employees',
@@ -33,10 +38,29 @@ app.use(
   authRoutes
 );
 
-app.listen(process.env.PORT, () => {
+/* ===================================== */
+/* ROOT ROUTE */
+/* ===================================== */
 
-  console.log(
-    `Server Running On Port ${process.env.PORT}`
+app.get('/', (req, res) => {
+
+  res.send(
+    'Vizhuthugal Backend Running Successfully 🚀'
   );
 
 });
+
+/* ===================================== */
+/* SERVER */
+/* ===================================== */
+
+app.listen(
+  process.env.PORT || 5000,
+  () => {
+
+    console.log(
+      `Server Running On Port ${process.env.PORT}`
+    );
+
+  }
+);
