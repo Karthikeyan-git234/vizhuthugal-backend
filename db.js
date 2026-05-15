c18caab7b160b636e1669c9052e7874e4e0c533a
Karthikeyan <karthikeyanagaram2004@gmail.com>
2026-05-15 23:16:34 +0530

    fixed neon database connection

--- db.js
+++ db.js
@@ -4,15 +4,12 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
 
-  host: process.env.DB_HOST,
+  connectionString:
+    process.env.DATABASE_URL,
 
-  user: process.env.DB_USER,
-
-  password: process.env.DB_PASSWORD,
-
-  database: process.env.DB_NAME,
-
-  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 
 });
 
